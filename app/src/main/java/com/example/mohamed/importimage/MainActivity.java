781cbe0860352fcbce6d84d01903c4c76b900e2c
Mohammed-Benotmane <mohammedbenotmanetom@gmail.com>
2018-07-31 04:06:14 +0200

    add project

---
+++ app/src/main/java/com/example/mohamed/importimage/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.mohamed.importimage;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private static int RESULT_LOAD_IMAGE = 1;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonLoadImage = (Button) findViewById(R.id.button);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent gallery = new Intent(Intent.ACTION_GET_CONTENT);
+                gallery.setType("image/*");
+                startActivityForResult(gallery, RESULT_LOAD_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            imageView = findViewById(R.id.imageView);
+            imageView.setImageURI(imageUri);
+
+        }
+    }
+}
